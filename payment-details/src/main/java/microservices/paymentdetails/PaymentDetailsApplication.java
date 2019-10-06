2e9410321d357ac3ce567137b6225b320cb01db5
lessam07 <47540189+lessam07@users.noreply.github.com>
2019-10-06 22:05:06 +0600

    Added discovery service

--- payment-details/src/main/java/microservices/paymentdetails/PaymentDetailsApplication.java
+++ payment-details/src/main/java/microservices/paymentdetails/PaymentDetailsApplication.java
@@ -2,8 +2,10 @@ package microservices.paymentdetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentDetailsApplication {
 
 	public static void main(String[] args) {
